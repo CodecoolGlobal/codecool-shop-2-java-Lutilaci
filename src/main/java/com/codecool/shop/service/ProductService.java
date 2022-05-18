44c44b7c95b635ce1c9845a5aa0e1ce9b28986e3
ekovattaa <ekovacsattila@gmail.com>
2022-05-18 13:34:26 +0200

    fix: remove unused import

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,11 +2,8 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
-
 import java.util.List;
 
 public class ProductService{
