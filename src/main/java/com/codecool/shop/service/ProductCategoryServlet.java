8e82e3d55f6232d444b231fe58ee3610d2a68cbc
bby2k <bakbence.2k@gmail.com>
2022-05-18 11:34:36 +0200

    Chores: Add imports

--- src/main/java/com/codecool/shop/service/ProductCategoryServlet.java
+++ src/main/java/com/codecool/shop/service/ProductCategoryServlet.java
@@ -1,7 +1,19 @@
 package com.codecool.shop.service;
 
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name="prodCatServlet", urlPatterns = {"/sg/something"}, loadOnStartup = 2)
 public class ProductCategoryServlet extends BasicServlet {
