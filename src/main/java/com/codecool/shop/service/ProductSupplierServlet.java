2c00578661b3d16d3a00472a27a41d2030123a5d
bby2k <bakbence.2k@gmail.com>
2022-05-18 11:39:52 +0200

    Feat: Remove duplicated index route

--- src/main/java/com/codecool/shop/service/ProductSupplierServlet.java
+++ src/main/java/com/codecool/shop/service/ProductSupplierServlet.java
@@ -3,7 +3,7 @@ package com.codecool.shop.service;
 
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name="prodSuppServlet", urlPatterns = {"/"}, loadOnStartup = 2)
+@WebServlet(name="prodSuppServlet", urlPatterns = {"/NOT-PER-REMOVE-LATER"}, loadOnStartup = 2)
 public class ProductSupplierServlet extends BasicServlet {
 
 
