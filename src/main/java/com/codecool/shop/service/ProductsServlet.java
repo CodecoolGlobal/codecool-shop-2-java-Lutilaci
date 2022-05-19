0e2dac01b3fbc97d2871609586f5b537fd0e8efc
bby2k <bakbence.2k@gmail.com>
2022-05-19 12:11:24 +0200

    Feat: Add ProductsServlet - An API route for all products

---
+++ src/main/java/com/codecool/shop/service/ProductsServlet.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.service;
+
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "allProduct",urlPatterns = {"/api/get/products"})
+public class ProductsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+
+        List<Product> products = productService.getAllProducts();
+
+        Gson gson = new Gson();
+        String json = "";
+        json += gson.toJson(products);
+        response.getOutputStream().print(json);
+    }
+}
