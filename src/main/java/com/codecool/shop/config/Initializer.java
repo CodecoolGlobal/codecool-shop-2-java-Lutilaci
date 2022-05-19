6f8994c74c1c4dbffc5cf5670c599291574758b5
bzari <brigitta.zari@gmail.com>
2022-05-19 17:23:34 +0200

    Refactor merge conflict problems

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -39,24 +39,24 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(kitchen);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Sunglass", new BigDecimal("12"), "USD", "For sunny days.", fun, amazon));
-        productDataStore.add(new Product("Candle Bulb", new BigDecimal("5"), "USD", "For candle-lit date nights.", home, lenovo));
-        productDataStore.add(new Product("Bottle Water", new BigDecimal("3"), "USD", "Definitely a thirst trap.", kitchen, amazon));
-        productDataStore.add(new Product("Self-watering can", new BigDecimal("20"), "USD", "Never run out of water.", home, amazon));
-        productDataStore.add(new Product("Aquarium Hourglass", new BigDecimal("15"), "USD", "Don't worry about time, but feeding your new pet.", fun, amazon));
-        productDataStore.add(new Product("Table Tennis equipment", new BigDecimal("8"), "USD", "There's a loophole.", fun, amazon));
-        productDataStore.add(new Product("Couple's mugs", new BigDecimal("30"), "USD", "For you and your clingy partner.", kitchen, amazon));
-        productDataStore.add(new Product("Double Champagne Glass", new BigDecimal("50"), "USD", "Double the fun on New Year's Eve.", kitchen, amazon));
-        productDataStore.add(new Product("Pocket Ruler", new BigDecimal("6"), "USD", "To measure every tiny detail.", home, amazon));
-        productDataStore.add(new Product("Balloon Paint Roller", new BigDecimal("45"), "USD", "Mix a birthday party with home renovation.", home, amazon));
-        productDataStore.add(new Product("Spherical dice", new BigDecimal("25"), "USD", "Test your luck.", fun, amazon));
-        productDataStore.add(new Product("Spork", new BigDecimal("10"), "USD", "If you can't decide.", kitchen, amazon));
-        productDataStore.add(new Product("SOUPer Bowl", new BigDecimal("12"), "USD", "Can't really hold liquid.", kitchen, amazon));
-        productDataStore.add(new Product("Rocking ladder", new BigDecimal("52"), "USD", "Swing by the roof", home, amazon));
-        productDataStore.add(new Product("Glass", new BigDecimal("22"), "USD", "For fashion, not prescription.", fun, amazon));
-        productDataStore.add(new Product("Self-hammer", new BigDecimal("35"), "USD", "You're gonna nail the home reno.", home, amazon));
-        productDataStore.add(new Product("Feather Knife", new BigDecimal("25"), "USD", "Chicks love it.", kitchen, amazon));
-        productDataStore.add(new Product("Booth trush", new BigDecimal("9"), "USD", "Clean 'em from a different angle.", fun, amazon));
+        productDataStore.add(new Product("Sunglass", new BigDecimal("12"), "USD", "For sunny days.", fun, CycleOps));
+        productDataStore.add(new Product("Candle Bulb", new BigDecimal("5"), "USD", "For candle-lit date nights.", home, CycleOps));
+        productDataStore.add(new Product("Bottle Water", new BigDecimal("3"), "USD", "Definitely a thirst trap.", kitchen, LooneyTools));
+        productDataStore.add(new Product("Self-watering can", new BigDecimal("20"), "USD", "Never run out of water.", home, LooneyTools));
+        productDataStore.add(new Product("Aquarium Hourglass", new BigDecimal("15"), "USD", "Don't worry about time, but feeding your new pet.", fun, LooneyTools));
+        productDataStore.add(new Product("Table Tennis equipment", new BigDecimal("8"), "USD", "There's a loophole.", fun, CycleOps));
+        productDataStore.add(new Product("Couple's mugs", new BigDecimal("30"), "USD", "For you and your clingy partner.", kitchen, LooneyTools));
+        productDataStore.add(new Product("Double Champagne Glass", new BigDecimal("50"), "USD", "Double the fun on New Year's Eve.", kitchen, CycleOps));
+        productDataStore.add(new Product("Pocket Ruler", new BigDecimal("6"), "USD", "To measure every tiny detail.", home, LooneyTools));
+        productDataStore.add(new Product("Balloon Paint Roller", new BigDecimal("45"), "USD", "Mix a birthday party with home renovation.", home, CycleOps));
+        productDataStore.add(new Product("Spherical dice", new BigDecimal("25"), "USD", "Test your luck.", fun, LooneyTools));
+        productDataStore.add(new Product("Spork", new BigDecimal("10"), "USD", "If you can't decide.", kitchen, CycleOps));
+        productDataStore.add(new Product("SOUPer Bowl", new BigDecimal("12"), "USD", "Can't really hold liquid.", kitchen, LooneyTools));
+        productDataStore.add(new Product("Rocking ladder", new BigDecimal("52"), "USD", "Swing by the roof", home, CycleOps));
+        productDataStore.add(new Product("Glass", new BigDecimal("22"), "USD", "For fashion, not prescription.", fun, LooneyTools));
+        productDataStore.add(new Product("Self-hammer", new BigDecimal("35"), "USD", "You're gonna nail the home reno.", home, CycleOps));
+        productDataStore.add(new Product("Feather Knife", new BigDecimal("25"), "USD", "Chicks love it.", kitchen, CycleOps));
+        productDataStore.add(new Product("Booth trush", new BigDecimal("9"), "USD", "Clean 'em from a different angle.", fun, LooneyTools));
 
 
     }
