b49fa182901beec53a7ce0c4c2baa5daba78bdaa
bby2k <bakbence.2k@gmail.com>
2022-05-19 16:50:04 +0200

    Chores: Add imports

--- src/main/java/com/codecool/shop/service/ProductsServlet.java
+++ src/main/java/com/codecool/shop/service/ProductsServlet.java
@@ -3,8 +3,10 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
 
