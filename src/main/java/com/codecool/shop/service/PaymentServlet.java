943dea41ad8607bd35116e4660d1bad7fb0bb007
ekovattaa <ekovacsattila@gmail.com>
2022-05-19 14:18:33 +0200

    feat: create new demo servlet with post method

--- src/main/java/com/codecool/shop/service/PaymentServlet.java
+++ src/main/java/com/codecool/shop/service/PaymentServlet.java
@@ -1,2 +1,23 @@
-package com.codecool.shop.service;public class PaymentServlet {
+package com.codecool.shop.service;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PaymentServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+        String name = req.getParameter("name");
+        String name = req.getParameter("name");
+        String name = req.getParameter("name");
+        String name = req.getParameter("name");
+        String name = req.getParameter("name");
+        String name = req.getParameter("name");
+
+        // TODO: create new object and set its fields from request!
+    }
 }
