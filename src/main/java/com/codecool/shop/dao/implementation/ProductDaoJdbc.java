4fbaf1fff919ec7bee81a566e3f5a0c2413e1403
ekovattaa <ekovacsattila@gmail.com>
2022-06-01 13:40:06 +0200

    feat: implement getAll and getCategoryById methods

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -6,6 +6,8 @@ import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoJdbc implements ProductDao {
@@ -18,7 +20,14 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public void add(Product product) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO products (prod_name, description, category_id, unit_price, currency, supplier_id) VALUES (?,?,?,?,?,?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, product.getName());
+            //statement.setString(2, product.);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -33,8 +42,34 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public List<Product> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM products";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            List <Product> products = new ArrayList<>();
+            while (rs.next()) {
+                Product product = new Product(rs.getString(1), rs.getBigDecimal(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+                products.add(product);
+            }
+            return products;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public getCategoryById(int id){
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT name, description FROM product_categories WHERE id = (?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+                    return new ProductCategory(rs.getString(1), rs.getString(2));
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public List<Product> getBy(Supplier supplier) {
