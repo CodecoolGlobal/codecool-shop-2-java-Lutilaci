9e92c31c0ddb9e2018ad2d2d48bdddc07914f234
bzari <brigitta.zari@gmail.com>
2022-06-01 13:57:53 +0200

    Implement getAll for supplier jdbc

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -1,9 +1,15 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoJdbc implements SupplierDao {
@@ -30,6 +36,18 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public List<Supplier> getAll() {
-        return null;
+        try(Connection conn = dataSource.getConnection()){
+            String sql = "SELECT name FROM suppliers";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            List<Supplier> supplierList = new ArrayList<>();
+            while (rs.next()){
+                Supplier supCat = new Supplier(rs.getString(1));
+                supplierList.add(supCat);
+            }
+            return supplierList;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading supplier with id: ", e);
+        }
     }
 }
