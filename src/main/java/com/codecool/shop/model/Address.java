3ae9c0e67339d5a8b13760ef09ee58d86e921ef4
bby2k <bakbence.2k@gmail.com>
2022-06-02 23:34:09 +0200

    Fix: For the Address class fix the access modifiers for the fields

--- src/main/java/com/codecool/shop/model/Address.java
+++ src/main/java/com/codecool/shop/model/Address.java
@@ -1,27 +1,64 @@
 package com.codecool.shop.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Address {
 
     // Data from shipping
     public int id;
-    public String firstName;
-    public String lastName;
-    public String street;
-    public String city;
-    public String zipCode;
 
-    public List<Product> productList;
+    private final int userId;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String zipCode;
+    private List<Product> productList;
 
-    public Address(String firstName, String lastName, String street, String city, String zipCode){
+    public Address(int userId, String firstName, String lastName, String street, String city, String zipCode){
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
         this.city = city;
         this.zipCode = zipCode;
+        productList = new ArrayList<>();
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProductToProductList(Product product){
+        productList.add(product);
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
