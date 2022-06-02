2ee8a506fe91fc008d49a9ba740c4b8419142276
bby2k <bakbence.2k@gmail.com>
2022-06-02 23:37:14 +0200

    Chores: Skeleton of the OrderDaoJdbc

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/OrderDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/OrderDaoJdbc.java
@@ -3,12 +3,25 @@ package com.codecool.shop.dao.implementation.jdbc;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.model.Order;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class OrderDaoJdbc implements OrderDao {
+    private DataSource dataSource;
+    public OrderDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void add(Order order) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id FROM users WHERE email = (?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
