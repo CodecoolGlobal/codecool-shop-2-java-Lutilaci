e8a0d5b8fa5f6c64246722c5fee6497b708e3d57
bby2k <bakbence.2k@gmail.com>
2022-06-02 23:41:39 +0200

    Feat: Implement User class

--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -6,4 +6,42 @@ public class User {
     private String email;
     private String password;
 
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("id: %1$d, " +
+                        "email: %2$s, " +
+                        "password: %3$s, ",
+                this.id,
+                this.email,
+                this.password);
+    }
 }
