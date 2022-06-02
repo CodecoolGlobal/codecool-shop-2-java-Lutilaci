30bf176244062436f27fdea1983f963d81704130
bby2k <bakbence.2k@gmail.com>
2022-06-02 23:37:41 +0200

    Feat: Add getters to OrderItem Model

--- src/main/java/com/codecool/shop/model/OrderItem.java
+++ src/main/java/com/codecool/shop/model/OrderItem.java
@@ -12,4 +12,16 @@ public class OrderItem {
         this.amount = amount;
         this.order_id = order.getId();
     }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public double getSubtotal_price() {
+        return subtotal_price;
+    }
 }
